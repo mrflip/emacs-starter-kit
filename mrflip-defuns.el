c863c71a5e0ac39668f739e944e592465f8f700f
Philip (flip) Kromer <flip@infochimps.org>
2012-02-25 15:41:44 -0600

    path to wu-lign

--- mrflip-defuns.el
+++ mrflip-defuns.el
@@ -146,7 +146,7 @@ makes)."
 
 (defun wulign-region (start end arg)
   (interactive (list (region-beginning) (region-end) current-prefix-arg))
-  (shell-command-on-region start end "wu-lign" t t))
+  (shell-command-on-region start end "~/bin/wu-lign" t t))
 
 
 (defun force-save ()
